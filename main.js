6dcf3947e8ab69f1df91edf0b166b1d4e07277ed
Pastel Pink <58058699+Majikku@users.noreply.github.com>
2021-07-08 20:57:38 +1200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const { Client } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+    console.log("IM ALIVE!");
+});
+
+client.login("ODYyNDQyMTIyNzU4MDYyMTQw.YOYZ0Q.gFI24sOqMqCFCWX_0taz29lzZxw")
