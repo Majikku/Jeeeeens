86586cbd4b1c20c0811abea4af655a6347aaa242
Pastel Pink <58058699+Majikku@users.noreply.github.com>
2021-07-08 21:09:08 +1200

    External Token

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const { Client } = require('discord.js');
 const client = new Client();
+const token = require("./token.json")
+//Change token-format.json file name to "token.json", or just chnage the file path.
 
 client.on('ready', () => {
     console.log("IM ALIVE!");
 });
 
-client.login("ODYyNDQyMTIyNzU4MDYyMTQw.YOYZ0Q.gFI24sOqMqCFCWX_0taz29lzZxw")
+client.login(token)
